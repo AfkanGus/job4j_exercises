cc84602315030c8ae78aa682e976966951b4cc22
afkan_guseynov <af.kan@bk.ru>
2023-02-25 23:20:33 +0400

    git

---
+++ src/main/java/ru/job4j/HelloWorld.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+/**
+ * Каждый оператор в Java должен заканчиваться точкой с запятой.
+ * Исправьте ошибку в приведенном коде.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
