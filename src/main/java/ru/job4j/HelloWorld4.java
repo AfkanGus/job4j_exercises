6b5c747820ae94595d55e9826c11609543af2b81
AfkanGus <af.kan@bk.ru>
2023-03-12 21:57:29 +0400

    commit

---
+++ src/main/java/ru/job4j/HelloWorld4.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+/**
+ * 4. Не закрытый метод
+ * В методе main  забыли закрывающую скобку. Поправь код
+ * приведенный ниже.
+ */
+public class HelloWorld4 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
---
+++ src/test/java/ru/job4j/HelloWorld4Test.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class HelloWorld4Test {
+    @Test
+    public void checkOutHelloJob4() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        HelloWorld4.main(null);
+        assertThat(out.toString(), is("Hello, Job4j!" + System.lineSeparator()));
+    }
+}
