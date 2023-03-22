fe0a4e19f1f19177fc4227e82d5945509f6187a2
AfkanGus <af.kan@bk.ru>
2023-03-22 20:54:06 +0800

    15. Вывод на консоль надписи

---
+++ src/main/java/ru/job4j/Task15.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+/**
+ * 15. Вывод на консоль надписи "+++!!!!??" (1)
+ * Дана строка +!?
+ * Напишите класс, который выводит на экран в первой строке три знака «+»,
+ * во второй ― четыре знака «!», в третьей ― два знака «?».
+ */
+public class Task15 {
+    public static void main(String[] args) {
+        String source = "+!?";
+        System.out.print(source.charAt(0));
+        System.out.print(source.charAt(0));
+        System.out.println(source.charAt(0));
+        System.out.print(source.charAt(1));
+        System.out.print(source.charAt(1));
+        System.out.print(source.charAt(1));
+        System.out.println(source.charAt(1));
+        System.out.print(source.charAt(2));
+        System.out.print(source.charAt(2));
+    }
+}
