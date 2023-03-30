4a5d4cea2dd9aefb3783f94dde13df79e73f5c30
afkan_guseynov <af.kan@bk.ru>
2023-03-30 18:01:17 +0400

    4. Целочисленное деление

---
+++ src/main/java/type/DivMod.java
@@ -0,0 +1,14 @@
+package type;
+
+/**
+ * 4. Целочисленное деление
+ */
+public class DivMod {
+    public static void main(String[] args) {
+        int num = 11;
+        int div = 3;
+        int mod = 11%3;
+        System.out.println(div);
+        System.out.println(mod);
+    }
+}
---
+++ src/test/java/type/DivModTest.java
@@ -0,0 +1,22 @@
+package type;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+public class DivModTest {
+    @Test
+    public void test() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        DivMod.main(null);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "3" + ln +
+                        "2" + ln
+        ));
+    }
+}
