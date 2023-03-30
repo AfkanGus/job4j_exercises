2fbb95c382e010a19e74f85b0734f9c7fb9fe7b8
afkan_guseynov <af.kan@bk.ru>
2023-03-30 18:26:13 +0400

    9. Сумма, разность, произведение, частное

---
+++ src/main/java/type/Task9.java
@@ -0,0 +1,15 @@
+package type;
+
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
