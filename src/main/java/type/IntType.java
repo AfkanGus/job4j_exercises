d12218fec4d5f1c6b81ece50b8769f98d998b652
afkan_guseynov <af.kan@bk.ru>
2023-03-30 17:35:46 +0400

    1. Объявление переменной

---
+++ src/main/java/type/IntType.java
@@ -0,0 +1,14 @@
+package type;
+
+import java.util.Scanner;
+
+/**
+ * 1. Объявление переменной.
+ */
+public class IntType {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int value = in.nextInt();
+        System.out.println(value);
+    }
+}
