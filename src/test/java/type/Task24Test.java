eb16dd41a09c0faa069577e11dae8da337b6689e
afkan_guseynov <af.kan@bk.ru>
2023-06-23 22:59:11 +0400

    : 24. Вывести двузначное число в полном виде

---
+++ src/test/java/type/Task24Test.java
@@ -0,0 +1,42 @@
+package type;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+;
+
+public class Task24Test {
+    public String ln = System.lineSeparator();
+
+    @Test
+    public void when35Then30Plus5() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task24.separate(35);
+        String expected = "30+5" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+
+    @Test
+    public void when19Then10Plus9() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task24.separate(19);
+        String expected = "10+9" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+
+    @Test
+    public void when63Then60Plus3() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task24.separate(63);
+        String expected = "60+3" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+}
