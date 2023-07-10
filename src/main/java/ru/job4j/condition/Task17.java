3a42b6b5a0f2e3e29128a859b7af11f7505e64b0
AfkanGus <af.kan@bk.ru>
2023-07-11 00:45:52 +0400

    17.вÑВывÐести знак числа

--- src/main/java/ru/job4j/condition/Task17.java
+++ src/main/java/ru/job4j/condition/Task17.java
@@ -9,12 +9,29 @@ package ru.job4j.condition;
  */
 public class Task17 {
     public static void signCheck(int number) {
-        if (number > 0) {
+       /* if (number > 0) {
             System.out.println("Положительное");
         } else if (number < 0) {
             System.out.println("Отрицательное");
         } else {
             System.out.println("Ноль");
-        }
+        }*/
+        /*String sign = (number == 0) ? "Ноль" : (number > 0) ? "Положительное" : "Отрицательное";
+        System.out.println(sign);
+        */
+       /* switch (Integer.signum(number)) {
+            case 1:
+                System.out.println("Положительное");
+                break;
+            case -1:
+                System.out.println("Отрицательное");
+                break;
+            default:
+                System.out.println("Ноль");
+                break;
+        }*/
+        String[] sigh = {"Положительное", "Отрицательное", "Ноль"};
+        int index = (number > 0) ? 0 : (number < 0) ? 1 : 2;
+        System.out.println(sigh[index]);
     }
 }
