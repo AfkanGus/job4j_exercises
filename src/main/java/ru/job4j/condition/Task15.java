d984ca33344a78b9d60ecfaf7447a911bb688680
AfkanGus <af.kan@bk.ru>
2023-07-11 00:55:18 +0400

    15. Увеличить число вдвоеÐ

--- src/main/java/ru/job4j/condition/Task15.java
+++ src/main/java/ru/job4j/condition/Task15.java
@@ -9,12 +9,22 @@ package ru.job4j.condition;
  */
 public class Task15 {
     public static void multiplyByTwo(int number) {
-        if (number > 0) { //если оно опложит
+       /* if (number > 0) { //если оно опложит
             int rsl = number * 2; //увеличить вдвое
             System.out.println(rsl); // и на консоль
         } else {
             System.out.println(number);
             //если не положительное, вывести без изменений
+        }*/
+       /* int index = (number > 0) ? number * 2 : number;
+        System.out.println(index);*/
+        switch (Integer.signum(number)) {
+            case 1:
+                int rsl = number * 2;
+                System.out.println(rsl);
+                break;
+            default:
+                System.out.println(number);
         }
     }
 }
