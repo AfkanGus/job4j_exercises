6ec84c7f5cdab6678f0a3f71d2e526cf8a96195a
AfkanGus <af.kan@bk.ru>
2023-07-24 18:33:03 +0400

    19. Вывести положительные трехзначные числа, оканчивающиеся на 7

--- src/main/java/ru/job4j/loop/Task19.java
+++ src/main/java/ru/job4j/loop/Task19.java
@@ -18,8 +18,13 @@ public class Task19 {
     public static void loop() {
         StringJoiner stringJoiner = new StringJoiner(" ");
         System.out.println("Старт");
-        for (int i = 107; i <= 147; i += 10) {
+       /* for (int i = 107; i <= 147; i += 10) {
                 stringJoiner.add(String.valueOf(i));
+        }*/
+        for (int i = 107; i <= 147; i++) {
+            if (i % 10 == 7) {
+                stringJoiner.add(String.valueOf(i));
+            }
         }
         System.out.println(stringJoiner);
         System.out.println("Финиш");
