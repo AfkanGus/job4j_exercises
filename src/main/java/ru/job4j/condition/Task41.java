23d2ea47a265499e81aa0bee86e390172631793b
AfkanGus <af.kan@bk.ru>
2023-07-28 19:41:34 +0400

    41. Найти количество четных цифр трехзначного числа

--- src/main/java/ru/job4j/condition/Task41.java
+++ src/main/java/ru/job4j/condition/Task41.java
@@ -14,7 +14,18 @@ package ru.job4j.condition;
  */
 public class Task41 {
     public static void evenDigitsAmount(int number) {
+        String numStr = String.valueOf(number); // Преобразуем число в строку
+        String[] digits = numStr.split(""); // Разделяем строку на отдельные символы
+
         int count = 0;
+        for (String digitStr : digits) {
+            int digit = Integer.parseInt(digitStr); // Преобразуем символ обратно в число
+            if (digit % 2 == 0) { // Проверяем, является ли цифра четной
+                count++;
+            }
+        }
+        System.out.println(count);
+     /*   int count = 0;
         while (number > 0) {
             int digit = number % 10;
             if (digit % 2 == 0) {
@@ -22,7 +33,7 @@ public class Task41 {
             }
             number /= 10;
         }
-        System.out.println(count);
+        System.out.println(count);*/
 
        /* int count = 0;
         int f = number / 100;
