3bdf08fe9af7ac4606f533b114f79ec95cb6364f
AfkanGus <af.kan@bk.ru>
2023-08-11 14:53:24 +0400

    FileOutPut

--- src/main/java/ru/job4j/exam/collections/generics/io/ex/ResultFileEx.java
+++ src/main/java/ru/job4j/exam/collections/generics/io/ex/ResultFileEx.java
@@ -5,9 +5,17 @@ import java.io.FileOutputStream;
 public class ResultFileEx {
     /*Напишем программу которая записывает текст в файл*/
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("data/dataEx.txt")) {
-            out.write("Hell".getBytes());
-            out.write(System.lineSeparator().getBytes());
+        try (FileOutputStream out = new FileOutputStream("data/tableUm")) {
+            int[][] array = new int[10][10];
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array.length; j++) {
+                    array[i][j] = (i + 1) * (j + 1);
+                    String str = Integer.toString(array[i][j]);
+                    out.write(str.getBytes());
+                    out.write(" ".getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
