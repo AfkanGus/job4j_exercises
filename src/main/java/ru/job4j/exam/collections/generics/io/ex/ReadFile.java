42a6f481fa83c71c4e8b4fc0b85ac3777d8953b6
AfkanGus <af.kan@bk.ru>
2023-08-12 16:41:10 +0400

    FileInputStream

--- src/main/java/ru/job4j/exam/collections/generics/io/ex/ReadFile.java
+++ src/main/java/ru/job4j/exam/collections/generics/io/ex/ReadFile.java
@@ -4,16 +4,18 @@ import java.io.FileInputStream;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("data/input.txt")) {
+        try (FileInputStream in = new FileInputStream("data/event.txt")) {
             StringBuilder txt = new StringBuilder();
             int read;
             while ((read = in.read()) != -1) {
                 txt.append((char) read);
             }
-            String[] lines = txt.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                System.out.println(line);
+            String[] num = txt.toString().split(System.lineSeparator());
+            for (String numbers : num) {
+                boolean rsl = (Integer.parseInt(numbers) % 2 == 0);
+                System.out.println(rsl);
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
