c3b1b5e44117e22a5eee18ce7f4f8f2fc5cdaffe
AfkanGus <af.kan@bk.ru>
2023-08-19 18:37:12 +0400

    51. Определить тип дроби

--- src/main/java/ru/job4j/condition/Task51.java
+++ src/main/java/ru/job4j/condition/Task51.java
@@ -20,6 +20,12 @@ package ru.job4j.condition;
  */
 public class Task51 {
     public static void fractionType(int num1, int num2) {
-
+        if (num1 > num2) {
+            System.out.println("Неправильная");
+        } else if (num1 < num2) {
+            System.out.println("Правильная");
+        } else {
+            System.out.println("Целое число");
+        }
     }
 }
