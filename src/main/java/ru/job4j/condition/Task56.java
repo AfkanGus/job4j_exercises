983bb6be96213b8c8ffe71a1c121bb19de3d64e5
AfkanGus <af.kan@bk.ru>
2023-08-27 10:03:08 +0400

    56. Определить возможность существования треугольника по сторонам

--- src/main/java/ru/job4j/condition/Task56.java
+++ src/main/java/ru/job4j/condition/Task56.java
@@ -17,7 +17,10 @@ package ru.job4j.condition;
  */
 public class Task56 {
     public static void isTriangle(int a, int b, int c) {
-
-
+        if (a < b + c && b < a + c && c < a + b) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
     }
 }
