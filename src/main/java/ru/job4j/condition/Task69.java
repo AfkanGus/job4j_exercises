2f9a69d56c944678d2da83c8838fe966aa34575c
AfkanGus <af.kan@bk.ru>
2023-09-03 11:58:52 +0500

    69. Определить фигуру по длинам сторон

--- src/main/java/ru/job4j/condition/Task69.java
+++ src/main/java/ru/job4j/condition/Task69.java
@@ -14,10 +14,12 @@ package ru.job4j.condition;
  */
 public class Task69 {
     public static void squareOrRectangle(int a, int b) {
-        if (a == b) {
+        String figure = (a == b) ? "Квадрат" : "Прямоугольник";
+        System.out.println(figure);
+      /*  if (a == b) {
             System.out.println("Квадрат");
         } else {
             System.out.println("Прямоугольник");
-        }
+        }*/
     }
 }
