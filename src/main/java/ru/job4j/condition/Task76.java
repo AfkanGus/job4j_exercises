0417e978e4ac3d6e59e77927b9fecab3ebb7122b
afkan_guseynov <af.kan@bk.ru>
2023-09-09 11:45:30 +0400

    5. Количество одинаковых цифр в трехзначном числе

--- src/main/java/ru/job4j/condition/Task76.java
+++ src/main/java/ru/job4j/condition/Task76.java
@@ -12,6 +12,26 @@ package ru.job4j.condition;
  */
 public class Task76 {
     public static void sameNumAmount(int num) {
+        String strNum = Integer.toString(num);
+        int count = 0;
 
+        for (int i = 0; i < strNum.length(); i++) {
+            char currentChar = strNum.charAt(i);
+            int charCount = 0;
+
+            for (int j = 0; j < strNum.length(); j++) {
+                if (strNum.charAt(j) == currentChar) {
+                    charCount++;
+                }
+            }
+
+            if (charCount > 1) {
+                count++;
+                // Чтобы избежать дублирования одинаковых цифр, заменяем их на символ, который не встречается в числе.
+                strNum = strNum.replace(currentChar, ' ');
+            }
+        }
+
+        System.out.println(count);
     }
 }
