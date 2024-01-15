1fa7617251689de81f9f638fe2139bfa2f9a47b1
afkan_guseynov <af.kan@bk.ru>
2024-01-15 21:30:31 +0500

    3. Мой output.

---
+++ src/main/java/ru/job4j/oop/Task3.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+/**
+ * 3. Мой output.
+ * В этом упражнение необходимо создать метод output в классе Task3.
+ * <p>
+ * Метод должен быть с параметром типа String. Метод должен быть не статический и типа void.
+ */
+public class Task3 {
+    public void output(String message) {
+
+    }
+}
