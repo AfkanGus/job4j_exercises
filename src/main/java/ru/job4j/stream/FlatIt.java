0d13005752b0c2d9e640beb6008bf0eba89f2c64
AfkanGus <af.kan@bk.ru>
2024-01-22 23:44:45 +0500

    1.0. flatMap для итератора

---
+++ src/main/java/ru/job4j/stream/FlatIt.java
@@ -0,0 +1,31 @@
+package ru.job4j.stream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * 1.0. flatMap для итератора
+ * Даны вложенные итераторы. Нужно написать метод,
+ * который соберет все элементы данных итераторов в List.
+ * <p>
+ * Для преобразование итератора в стрим нужно воспользоваться
+ * методом iteratorToStream(). Для решения нужно также
+ * использовать методы flatMap() и collect().
+ */
+public class FlatIt {
+    public static List<Integer> flatten(Iterator<Iterator<Integer>> it) {
+        return iteratorToStream(it)
+                .flatMap(FlatIt::iteratorToStream)
+                .collect(Collectors.toList());
+    }
+
+    private static <T> Stream<T> iteratorToStream(Iterator<T> it) {
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(it, Spliterator.ORDERED),
+                false);
+    }
+}
