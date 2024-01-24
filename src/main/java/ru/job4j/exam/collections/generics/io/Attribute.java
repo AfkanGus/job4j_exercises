5e7f5926e75acd469d91e77071b8fee79b7b9554
afkan_guseynov <af.kan@bk.ru>
2024-01-24 21:31:00 +0500

    32. Как отправить через сокет сообщение

--- src/main/java/ru/job4j/exam/collections/generics/io/Attribute.java
+++ src/main/java/ru/job4j/exam/collections/generics/io/Attribute.java
@@ -8,16 +8,18 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class Attribute {
     public static void main(String[] args) throws IOException {
-     /*   Path file = Path.of("Attributes.txt");
+       Path file = Path.of("Attributes.txt");
         Files.createFile(file);
         BasicFileAttributeView basicFileAttributeView = Files.getFileAttributeView(file, BasicFileAttributeView.class);
         BasicFileAttributes attributes = basicFileAttributeView.readAttributes();
         System.out.println(attributes);
+
         Path file1 = Path.of("Attributes1.txt");
         Files.createFile(file1);
         BasicFileAttributes attributes1 = Files.readAttributes(file1, BasicFileAttributes.class);
-        System.out.println(attributes1);*/
-        Path file3 = Path.of("Attributes3");
+        System.out.println(attributes1);
+
+       /* Path file3 = Path.of("Attributes3");
         Files.createFile(file3);
         BasicFileAttributes attributes3 = Files.readAttributes(file3, BasicFileAttributes.class);
         System.out.println(attributes3.getClass());
@@ -29,6 +31,6 @@ public class Attribute {
         System.out.println("size " + attributes3.size());
         System.out.println("Время последнего доступа: " + attributes3.lastAccessTime());
         System.out.println("Время последнего изменения: " + attributes3.lastModifiedTime());
-        System.out.println(attributes3.getClass());
+        System.out.println(attributes3.getClass());*/
     }
 }
