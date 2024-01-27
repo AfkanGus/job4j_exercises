408c40cb9363776515df6749a4aa76ad6c5bdc76
AfkanGus <af.kan@bk.ru>
2024-01-27 17:43:52 +0500

    1.1. Создание стрима. Метод stream()

--- src/main/java/ru/job4j/stream/StreamMethod.java
+++ src/main/java/ru/job4j/stream/StreamMethod.java
@@ -1,9 +1,19 @@
 package ru.job4j.stream;
+
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamMethod {
     public static Stream<Integer> createStream(Collection<Integer> data) {
         return data.stream();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        Stream<Integer> stream = StreamMethod.createStream(numbers);
+        stream.filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
     }
 }
