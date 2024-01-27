0587d082ca7745d50ea29e84f2233d9d5bdcd7e8
AfkanGus <af.kan@bk.ru>
2024-01-27 17:22:00 +0500

    check

---
+++ src/main/java/ru/job4j/stream/StreamMethod.java
@@ -0,0 +1,10 @@
+package ru.job4j.stream;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public class StreamMethod {
+    public static Stream<Integer> createStream(Collection<Integer> data) {
+        return data.stream();
+    }
+}
