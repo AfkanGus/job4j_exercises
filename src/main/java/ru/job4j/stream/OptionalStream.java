88221c1ae8cb804141b41eeb3bbd23bde2be0ec5
afkan_guseynov <af.kan@bk.ru>
2024-02-16 20:16:54 +0500

    7.0. Optional и Stream. Метод stream()

---
+++ src/main/java/ru/job4j/stream/OptionalStream.java
@@ -0,0 +1,52 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 7.0. Optional и Stream. Метод stream()
+ * Ваша задача найти тел. номера пользователя с заданным id, которые начинаются с region.
+ */
+public class OptionalStream {
+    public static class PhoneNumber {
+
+        private String phone;
+
+        public PhoneNumber(String phone) {
+            this.phone = phone;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+    }
+
+    public static class User {
+
+        private int id;
+
+        private List<PhoneNumber> numbers;
+
+        public User(int id, List<PhoneNumber> numbers) {
+            this.id = id;
+            this.numbers = numbers;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public List<PhoneNumber> getNumbers() {
+            return numbers;
+        }
+    }
+
+    public static List<PhoneNumber> collectNumber(List<User> users, int id, String region) {
+        return users.stream()
+                .filter(user -> user.getId() == id)
+                .flatMap(user -> user.getNumbers().stream())
+                .filter(phoneNumber -> phoneNumber.getPhone().startsWith(region))
+                .collect(Collectors.toList()); // Находим первый подходящий телефонный номер
+    }
+}
