12ab64d6b632786a8d12d625e6ec8080cb8ad8e5
AfkanGus <af.kan@bk.ru>
2024-03-14 00:21:03 +0500

    Zadacha

--- src/main/java/ru/job4j/PrioritetOchered.java
+++ src/main/java/ru/job4j/PrioritetOchered.java
@@ -6,7 +6,7 @@ public class PrioritetOchered {
     private  LinkedList<Zadachi> spisokZadach = new LinkedList<>();
 
     public void put(Zadachi zadachi) {
-        int index = 0;
+        int index = 0;  /*куда вставить задачу, 0 -  нов задача вставл в начало если имеет наивсш приорт*/
         for (Zadachi currentZadacha : spisokZadach) {
             if (zadachi.getPrioritet() < currentZadacha.getPrioritet()) {
                 break;
